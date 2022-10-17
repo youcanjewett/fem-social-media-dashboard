38ae4e282cfad887d3896b93733dae0db783f1a4
katie <kjewett17@gmail.com>
2022-10-16 19:44:53 -0500

    toggle functionality
    adding toggle functionality with js
    implementing dark/light color variables to be set with js functions

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,33 @@
-console.log("hello")
+const darkButton = document.getElementById("dark");
+const lightButton = document.getElementById("light");
+const radioButtons = document.querySelectorAll(".toggle__wrapper input");
+
+const setDarkMode = () => {
+  document.querySelector("body").classList = "dark";
+  localStorage.setItem("colorMode", "dark");
+};
+
+const setLightMode = () => {
+  document.querySelector("body").classList = "light";
+  localStorage.setItem("colorMode", "light");
+};
+
+radioButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    darkButton.checked ? setDarkMode() : setLightMode();
+  });
+});
+
+const colorModeFromPreferences = () => {
+  return window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light"; // If preference is set or does not match anything (light is default)
+};
+
+const loadAndUpdateColor = () => {
+  // setting the theme toggle to default preference
+  const color = colorModeFromPreferences();
+  color == "dark" ? darkButton.click() : lightButton.click();
+};
+
+loadAndUpdateColor();
